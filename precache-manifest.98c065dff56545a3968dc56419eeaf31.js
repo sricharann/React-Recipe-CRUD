0db67563aff715f2059a1896a7870f8f63b3628e
sricharann <sricharan034@gmail.com>
2023-01-03 00:08:15 -0500

    Updates

---
+++ precache-manifest.98c065dff56545a3968dc56419eeaf31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b6e219c462f27f50dd3d1a68daad04",
+    "url": "/React-Recipe-CRUD/index.html"
+  },
+  {
+    "revision": "7deb3b461010b383a174",
+    "url": "/React-Recipe-CRUD/static/css/main.cd8500c1.chunk.css"
+  },
+  {
+    "revision": "f23a3b5c019bd619e369",
+    "url": "/React-Recipe-CRUD/static/js/2.034f5327.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Recipe-CRUD/static/js/2.034f5327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7deb3b461010b383a174",
+    "url": "/React-Recipe-CRUD/static/js/main.8907cf19.chunk.js"
+  },
+  {
+    "revision": "751894afa26c577221c6",
+    "url": "/React-Recipe-CRUD/static/js/runtime-main.12770bf6.js"
+  }
+]);
